8a173b2b6d1bd84edb7d83ebd04507f33a0fa887
Einar Quizon Stavnes <estavnes@gmail.com>
2025-06-20 01:17:26 +0200

    First commit

---
+++ daxfmt/main.py
@@ -0,0 +1,41 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    url = "https://www.daxformatter.com/?embed=1"
+
+    if sys.stdin.isatty():
+        print("No content given")
+        return
+
+    dax_code = sys.stdin.read().strip()
+
+    if not dax_code:
+        print("No content given")
+        return
+
+    data = {
+        'r': 'US',
+        'fx': dax_code
+    }
+
+    response = requests.post(url, data=data)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        formatted_div = soup.find('div', {'class': 'formatted'})
+        if formatted_div:
+            for br in formatted_div.find_all('br'):
+                br.replace_with('\n')
+            text = formatted_div.get_text().replace('\xa0', ' ')
+            print(text)
+        else:
+            print("Could not find formatted DAX.", file=sys.stderr)
+    else:
+        print("Error:", response.status_code, response.text, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
